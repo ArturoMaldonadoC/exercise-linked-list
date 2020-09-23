46e5038d4159c8d12ac9332c8d991979e9b511fd
ArturoMaldonadoC <arturo.99@live.com>
2020-09-23 13:43:15 -0500

    Estructura LinkedList creada

---
+++ src/uaslp/enginering/labs/list/LinkedList.java
@@ -0,0 +1,37 @@
+package uaslp.enginering.labs.list;
+
+public class LinkedList {
+
+    private Node front;
+    private Node tail;
+
+    public void add (Student student){
+
+    }
+
+    public void delete (Student student){
+
+    }
+
+    public void delete (int index){
+
+    }
+
+    public IteratorArrayList getIterator (){
+
+        return null;
+
+    }
+
+    public int size (){
+        return 0;
+    }
+
+    public Student getAt(int index){
+        return null;
+    }
+
+    public void insert (Student reference, Student newStudent){
+
+    }
+}
