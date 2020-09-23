c243c1e21c71a6e5f1177212fb67aebdecdfb0c1
ArturoMaldonadoC <arturo.99@live.com>
2020-09-23 14:55:17 -0500

    Estructura Node creada

---
+++ src/uaslp/enginering/labs/list/Node.java
@@ -0,0 +1,42 @@
+package uaslp.enginering.labs.list;
+
+public class Node {
+
+    private Student student;
+
+    private Node previous;  //Atributos
+
+    private Node next; // Atributos
+
+    public void setStudent(Student student){
+        this.student = student;
+
+    }
+
+    public Student getStudent(){
+        return student;
+    }
+
+    public void setPrevious(Node previous){
+
+        this.previous = previous;
+
+    }
+
+    public Node getPrevious(){
+
+        return previous;
+    }
+
+    public void setNext(Node next){
+
+        this.next = next;
+
+    }
+
+    public Node getNext(){
+        return next;
+    }
+
+
+}
